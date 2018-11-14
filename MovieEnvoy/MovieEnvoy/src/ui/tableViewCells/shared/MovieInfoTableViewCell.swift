051cfa3a8b26f9e5e31da8ed10bfd9ec11c786e2
Jerry Edens <ceciljedensjr@gmail.com>
2018-11-14 14:28:48 -0800

    added tabbar icons

---
+++ MovieEnvoy/MovieEnvoy/src/ui/tableViewCells/shared/MovieInfoTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieInfoTableViewCell.swift
+//  MovieEnvoy
+//
+//  Created by Jerry Edens on 11/14/18.
+//  Copyright © 2018 Edens R&D. All rights reserved.
+//
+
+import UIKit
+
+class MovieInfoTableViewCell: UITableViewCell {
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+}
