8c67124e85f8ae6466461e2c325e90e76b52eafc
Jerry Edens <ceciljedensjr@gmail.com>
2018-11-14 13:38:07 -0800

    added initial viewcontrollers

---
+++ MovieEnvoy/MovieEnvoy/src/ui/flow/popular/PopularViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PopularViewController.swift
+//  MovieEnvoy
+//
+//  Created by Jerry Edens on 11/14/18.
+//  Copyright © 2018 Edens R&D. All rights reserved.
+//
+
+import UIKit
+
+class PopularViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
---
+++ MovieEnvoy/MovieEnvoy/src/ui/flow/topRated/TopRatedViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TopRatedViewController.swift
+//  MovieEnvoy
+//
+//  Created by Jerry Edens on 11/14/18.
+//  Copyright © 2018 Edens R&D. All rights reserved.
+//
+
+import UIKit
+
+class TopRatedViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
